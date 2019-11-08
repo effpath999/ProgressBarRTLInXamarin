1205e11f32fc2a7fa0a22aafe3598f5b1878cd47
Francesco <effpath@outlook.com>
2019-11-08 18:04:38 +0100

    Aggiungere i file di progetto.

---
+++ TestProgressBar/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestProgressBar
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            TheProgressBar.Progress = 0;
+            await Task.Delay(500);
+            for (int k = 0; k <= 9; ++k)
+            {
+                TheProgressBar.Progress += .1;
+                await Task.Delay(500);
+            }
+        }
+    }
+}
